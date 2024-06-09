e331419d7f67d9599168be7cb15ab6bbe5425d2d
EverVino <evervino00@gmail.com>
2024-06-09 12:34:24 -0400

    update theme

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -19,12 +19,12 @@ keymap("v", "<", "<gv", opts)
 -- Mover el texto hacia arriba o haci abajo Alt + j o Alt + k
 keymap('v', '<a-j>', ':m .+1<cr>==', opts)
 keymap('v', '<a-k>', ':m .-2<cr>==', opts)
-keymap('v', 'p', '"_dP', opts)
+--keymap('v', 'p', '"_dP', opts) -- revisar la funcion de esta keymap
 
 --Visual Block mode
 --Mover el texto hacia arriba y hacia abajo
-keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 
