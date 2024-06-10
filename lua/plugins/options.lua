2ba4207eafa35edf47fd6f420e9c65e78005e839
EverVino <evervino00@gmail.com>
2024-06-10 10:37:30 -0400

    change theme options

--- lua/plugins/options.lua
+++ lua/plugins/options.lua
@@ -1,5 +1,5 @@
-vim.cmd("colorscheme gruvbox") --set colorscheme
-
+vim.cmd("colorscheme catppuccin") --set colorscheme
+vim.o.background = "dark"
 vim.opt.termguicolors = true --bufferline
 require("bufferline").setup{} --bufferline
 
