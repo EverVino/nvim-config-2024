d051b0c2355c8c57b7d16296495ba88116743028
EverVino <evervino00@gmail.com>
2024-06-11 19:01:20 -0400

    add indent folding

--- lua/options.lua
+++ lua/options.lua
@@ -27,3 +27,7 @@ vim.opt.clipboard = "unnamedplus"
 -- Marcar la linea actual de codificación
 vim.opt.cursorline = true
 
+vim.opt.foldmethod = "indent"
+vim.opt.foldtext = ""
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 1
